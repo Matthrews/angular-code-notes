17695fa0e0f1fe876fe0532bb9b4ba182c3f452e
Oliver <matthrews@outlook.com>
2023-07-02 13:45:10 +0800

    directive-demo-1: Export

--- directive-demo-1/src/app/app.component.ts
+++ directive-demo-1/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ export class AppComponent {
   title = 'directive-demo-1';
   authProfile = [ { r: 'admin_config', a: ['view'] } ]
   highlighted = true;
+
+  @ViewChild(HighlightDirective)
+  highlighter: HighlightDirective | undefined;
+
+  ngAfterViewInit(): void {
+    // AfterViewInit function
+    console.log('AfterViewInit', typeof this.highlighter?.toggle);
+  }
 
   public onToggleHighlight(isHighlight: boolean) {
     console.log('onToggleHighlight', isHighlight);
